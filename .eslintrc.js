7eb03fafb776751ea5b8defee624346d1f7c36ee
Víctor Santiago <vrsa9208@gmail.com>
2020-01-29 10:31:29 -0600

    :wrench: Initial React setup

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  plugins: ["react"],
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  env: {
+    node: true
+  },
+  settings: {
+    react: {
+      version: "16.4.2"
+    }
+  }
+};
