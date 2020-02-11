7f44cab356e526a737772ea812585cfa2ad8b43c
Víctor Santiago <vrsa9208@gmail.com>
2020-02-11 11:22:40 -0600

    :sparkles: Add auto-advance support to Carousel

--- src/Carousel.js
+++ src/Carousel.js
@@ -3,6 +3,7 @@ import CarouselButton from "./CarouselButton";
 import CarouselSlide from "./CarouselSlide";
 import PropTypes from "prop-types";
 import HasIndex from "./HasIndex";
+import AutoAdvances from "./AutoAdvances";
 
 export class Carousel extends PureComponent {
   static propTypes = {
@@ -58,4 +59,7 @@ export class Carousel extends PureComponent {
   }
 }
 
-export default HasIndex(Carousel, "slideIndex");
+export default HasIndex(
+  AutoAdvances(Carousel, "slideIndex", "slides"),
+  "slideIndex"
+);
