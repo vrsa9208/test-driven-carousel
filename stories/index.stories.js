da9764862baad3de4a6203581af9e811dd56f70a
Víctor Santiago <vrsa9208@gmail.com>
2020-02-12 09:57:41 -0600

    :wrench: Initial Storybook setup

---
+++ stories/index.stories.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import { linkTo } from "@storybook/addon-links";
+
+import { Button, Welcome } from "@storybook/react/demo";
+import Carousel from "../src/Carousel";
+import slides from "../example/slides";
+
+storiesOf("Welcome", module).add("to Storybook", () => (
+  <Welcome showApp={linkTo("Button")} />
+));
+
+storiesOf("Button", module)
+  .add("with text", () => (
+    <Button onClick={action("clicked")}>Hello Button</Button>
+  ))
+  .add("with some emoji", () => (
+    <Button onClick={action("clicked")}>
+      <span role="img" aria-label="so cool">
+        😀 😎 👍 💯
+      </span>
+    </Button>
+  ));
+
+storiesOf("Carousel", module).add("default", () => (
+  <Carousel slides={slides} />
+));
